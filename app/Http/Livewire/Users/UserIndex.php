57bff0d888a954d2150f9c18805257443e70ea3d
yeheskhielprasetyo <yeheskhielprasetyorakordana123@gmail.com>
2021-12-30 04:31:58 +0800

    new

---
+++ app/Http/Livewire/Users/UserIndex.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire\Users;
+
+use App\Models\User;
+use Livewire\Component;
+
+class UserIndex extends Component
+{
+    public $search = '';
+
+
+    public function render()
+    {
+        $users = User::all();
+        if (strlen($this->search) > 2) {
+            $users = User::where('username', 'like', "%{$this->search}%")->get();
+        }
+        return view('livewire.users.user-index', [
+            'users' => $users
+        ])
+            ->layout('layouts.main');
+    }
+}
---
+++ resources/views/livewire/users/user-index.blade.php
@@ -0,0 +1,67 @@
+<div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Users</h1>
+    </div>
+    <div class="row">
+        <div class="card  mx-auto">
+            <div>
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
+            </div>
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        <form method="GET" action="{{ route('users.index') }}">
+                            <div class="form-row align-items-center">
+                                <div class="col">
+                                    <input type="search" wire:model="search" name="search" class="form-control mb-2" id="inlineFormInput"
+                                    >
+                                </div>
+                                <div class="col" wire:loading>
+                                    <div class="spinner-border" role="status">
+                                        <span class="sr-only">Loading...</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <a href="" class="btn btn-primary mb-2">Create</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table" wire:loading.remove>
+                    <thead>
+                        <tr>
+                            <th scope="col">#Id</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Manage</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($users as $user)
+                            <tr>
+                                <th scope="row">{{ $user->id }}</th>
+                                <td>{{ $user->username }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <a href="" class="btn btn-success">Edit</a>
+                                </td>
+                            </tr>
+                            @empty
+                            <tr>
+                                <th>No Results</th>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
